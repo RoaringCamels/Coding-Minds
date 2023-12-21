479032c70a353866eafba1927bc93a4312ddfc5c
Ivan Revilla <revillaivan97@gmail.com>
2023-12-20 22:54:55 -0800

    Added chatgpt apie example from curriculum

---
+++ HS Prooject - Sales/ChatGPT API/chatgpt.py
@@ -0,0 +1,4 @@
+# Example of how to use th OpenAI API Client and create an API key
+# You can create a virtual environment first to keep the dependindies in this projet only
+
+# First install the OpenAI API Client using 'pip install openai
